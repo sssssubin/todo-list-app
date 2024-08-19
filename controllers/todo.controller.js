931461494d5e51792378f167120c4252a9c5aed8
forest-k <bstnqls@naver.com>
2024-08-19 23:33:05 +0900

    feat: 구현 - 할일 CRUD 기능 추가 (조회, 추가, 수정, 삭제)

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -9,6 +9,17 @@ const handleError = (res, error, statusCode = 400) => {
 // 모든 할일 가져오기(GET)
 const getTodos = async (req, res) => {
   try {
+    const todos = await Todo.find();
+    if (todos.length === 0) {
+      return res
+        .status(404)
+        .json({ status: "fail", message: "No todos found" });
+    }
+    if (req.headers.accept && req.headers.accept.includes("application/json")) {
+      return res.json(todos);
+    } else {
+      return res.render("index", { todos });
+    }
   } catch (error) {
     handleError(res, error);
   }
@@ -17,6 +28,13 @@ const getTodos = async (req, res) => {
 // 특정 할일 가져오기(GET)
 const getTodoById = async (req, res) => {
   try {
+    const todo = await Todo.findById(req.params.id);
+    if (!todo) {
+      return res
+        .status(404)
+        .json({ status: "fail", message: "Todo not found" });
+    }
+    return res.json(todo);
   } catch (error) {
     handleError(res, error);
   }
@@ -25,6 +43,10 @@ const getTodoById = async (req, res) => {
 // 할일 추가하기(POST)
 const createTodo = async (req, res) => {
   try {
+    const { title, category, content } = req.body;
+    const newTodo = new Todo({ title, category, content });
+    await newTodo.save();
+    return res.status(201).json(newTodo);
   } catch (error) {
     handleError(res, error);
   }
@@ -33,6 +55,15 @@ const createTodo = async (req, res) => {
 // 할일 수정하기(PUT)
 const editTodo = async (req, res) => {
   try {
+    const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!updatedTodo) {
+      return res
+        .status(404)
+        .json({ status: "fail", message: "Todo not found" });
+    }
+    return res.json(updatedTodo);
   } catch (error) {
     handleError(res, error);
   }
@@ -41,6 +72,13 @@ const editTodo = async (req, res) => {
 // 할일 삭제하기(DELETE)
 const deleteTodo = async (req, res) => {
   try {
+    const deletedTodo = await Todo.findByIdAndDelete(req.params.id);
+    if (!deletedTodo) {
+      return res
+        .status(404)
+        .json({ status: "fail", message: "Todo not found" });
+    }
+    return res.status(200).json({ status: "success", message: "Todo deleted" });
   } catch (error) {
     handleError(res, error);
   }
